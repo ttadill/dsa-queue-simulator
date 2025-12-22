8add08011293c1f0b0c74cf2d0bb7b49ff6130d9
ttadill <tadillatabhandari@gmail.com>
2025-12-22 23:38:50 +0545

    change

--- src/queue.py
+++ src/queue.py
@@ -1,31 +1,32 @@
 class Queue:
-    """Queue implementation using FIFO"""
+    #Queue implementation using FIFO. This queue will store vehicles in traffic lane
 
     def __init__(self):
+        #to store queue items
         self.items = []
 
     def enqueue(self, item):
-        """Add item to the queue"""
+        #Add item to the queue
         self.items.append(item)
 
     def dequeue(self):
-        """Remove and return front item"""
+        #Remove and return front item
         if self.is_empty():
             return None
         return self.items.pop(0)
 
     def peek(self):
-        """Return the front item without removing it"""
+        #Return the front item without removing it
         if self.is_empty():
             return None
         return self.items[0]
 
     def is_empty(self):
-        """Check if the queue is empty"""
+        #Check if the queue is empty
         return len(self.items) == 0
 
     def size(self):
-        """Return the number of items in the queue"""
+        #Return the number of items in the queue
         return len(self.items)
 
     def __str__(self):
