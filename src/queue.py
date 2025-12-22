7c217b7ea3a722440257a45eab16b87eee274385
ttadill <tadillatabhandari@gmail.com>
2025-12-22 23:17:18 +0545

    queue implementation

---
+++ src/queue.py
@@ -0,0 +1,32 @@
+class Queue:
+    """Queue implementation using FIFO"""
+
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        """Add item to the queue"""
+        self.items.append(item)
+
+    def dequeue(self):
+        """Remove and return front item"""
+        if self.is_empty():
+            return None
+        return self.items.pop(0)
+
+    def peek(self):
+        """Return the front item without removing it"""
+        if self.is_empty():
+            return None
+        return self.items[0]
+
+    def is_empty(self):
+        """Check if the queue is empty"""
+        return len(self.items) == 0
+
+    def size(self):
+        """Return the number of items in the queue"""
+        return len(self.items)
+
+    def __str__(self):
+        return f"Queue({self.items})"
