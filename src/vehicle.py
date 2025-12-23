bf6693de88f027112fe5b9201bd06ae9c81cc233
ttadill <tadillatabhandari@gmail.com>
2025-12-23 19:38:29 +0545

    vehicle

--- src/vehicle.py
+++ src/vehicle.py
@@ -3,5 +3,10 @@ class Vehicle:
     Represents a vehicle arriving at the traffic junction
     """
 
-    def __init__(self, vehicle_id):
+    def __init__(self, vehicle_id, priority=False):
         self.vehicle_id = vehicle_id
+        self.priority = priority
+
+    def __str__(self):
+        return f"Vehicle({self.vehicle_id}, priority={self.priority})"
+
