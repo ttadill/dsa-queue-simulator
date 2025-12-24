09ac168d0e9ee0da02d90e82951a8640614e2a2f
ttadill <tadillatabhandari@gmail.com>
2025-12-24 23:19:41 +0545

    modify

--- src/lane.py
+++ src/lane.py
@@ -1,22 +1,23 @@
 from src.queue import Queue
+from src.traffic_light import TrafficLight
+
 
 class Lane:
-    """
-    Represents a traffic lane containing a queue of vehicles
-    """
-
-    def __init__(self, name):
-        self.name = name
-        self.queue = Queue()
+    
+    def __init__(self, lane_id, queue):
+        self.lane_id = lane_id
+        self.queue = queue
 
     def add_vehicle(self, vehicle):
         self.queue.enqueue(vehicle)
 
     def process_vehicle(self):
+        if self.queue.is_empty():
+            return None
         return self.queue.dequeue()
 
     def is_empty(self):
         return self.queue.is_empty()
-    
+
     def queue_size(self):
         return self.queue.size()
