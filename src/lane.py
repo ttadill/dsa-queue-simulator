a6e5aaee18efbbec84b38fd089c21a252e05a67a
ttadill <tadillatabhandari@gmail.com>
2025-12-26 20:43:16 +0545

    update

--- src/lane.py
+++ src/lane.py
@@ -1,21 +1,20 @@
 from src.queue import Queue
 from src.traffic_light import TrafficLight
+from src.vehicle import Vehicle
 
 
 class Lane:
-
     def __init__(self, lane_id, queue):
         self.lane_id = lane_id
         self.queue = queue
         self.light = TrafficLight()
 
-
-    def add_vehicle(self, vehicle):
-    if vehicle.priority:
-        self.queue.items.insert(0, vehicle)
-    else:
-        self.queue.enqueue(vehicle)
-
+    def add_vehicle(self, vehicle: Vehicle):
+        if vehicle.priority:
+            # Priority vehicle goes to the front
+            self.queue.items.insert(0, vehicle)
+        else:
+            self.queue.enqueue(vehicle)
 
     def process_vehicle(self):
         if not self.light.is_green():
